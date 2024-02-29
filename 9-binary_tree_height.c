6e991378d7b9899a052f0bda0c617ca0f020cdbe
ongaro09 <ongaromark7@gmail.com>
2024-02-29 21:53:02 +0300

    fixed logical error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_height - measures the height of a binary tree
- *@tree: pointer to the root node of the tree to measure the height
+ *binary_tree_height - Measures the height of a binary tree.
+ *@tree: Pointer to the root node of the tree to measure the height.
  *
- *Return: height of the tree, 0 if tree is NULL
+ *Return: Height of the binary tree. If tree is NULL, return 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
